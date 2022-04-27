2b01fffa545ad34f432e30573ba3743be3828bc4
Igor Ferreira <me@igorcferreira.dev>
2022-04-28 01:16:56 +0200

    Fix dot files links

--- install_dot_files.sh
+++ install_dot_files.sh
@@ -8,4 +8,6 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 ln -s "${SCRIPT_PATH}/dotfiles/.bash_functions" "${HOME}/.bash_functions"
 ln -s "${SCRIPT_PATH}/dotfiles/.zprofile" "${HOME}/.zprofile"
 ln -s "${SCRIPT_PATH}/dotfiles/.zshrc" "${HOME}/.zshrc"
-ln -s "${SCRIPT_PATH}/dotfiles/.gitconfig" "${HOME}/.gitconfig"
+
+# We don't link the git config to avoid leaking sensitive data
+cp "${SCRIPT_PATH}/dotfiles/.gitconfig" "${HOME}/.gitconfig"
