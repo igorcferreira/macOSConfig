00913a81f8a1e4b9cce801b4c27a5d7079a88bc7
Igor Ferreira <me@igorcferreira.dev>
2023-11-21 12:15:24 +0100

    Update GPG key server

--- configure_ruby.sh
+++ configure_ruby.sh
@@ -8,7 +8,7 @@ if which rvm > /dev/null; then
 	echo "RVM already installed"
 else
 	if which gpg > /dev/null; then
-		gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+		gpg --keyserver hkps://keys.openpgp.org --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 	fi
 	curl -sSL https://get.rvm.io | bash -s stable --rails --ignore-dotfiles
 fi
